4675e4c3162af0ec3ddf14a02861b6fa1e90b4cc
LilShocko <52737404+LilShocko@users.noreply.github.com>
2019-07-25 17:13:23 +0300

    2nd

--- main.cpp
+++ main.cpp
@@ -1,161 +1,53 @@
 #include <iostream>
 using namespace std;
-class song{
+class ElectricalDevice{
 private:
-    int length;
-    char* artist;
-    char* jenre;
+    int avexp;
 public:
-    song(){
-        length=0;
-        artist= new char[1];
-        artist[0]='\0';
-        jenre= new char[1];
-        jenre[0]='\0';
-    }
-    song(int _length, char* _artist, char* _jenre){
-        length=_length;
-        int sizee=0;
-        for(int i=0; _artist[i]!='\0'; i++){
-            sizee++;
-        }
-        artist=new char[sizee];
-        for(int i=0; i<sizee; i++){
-            artist[i]=_artist[i];
-        }
-        sizee=0;
-        for(int i=0; _jenre[i]!='\0'; i++){
-            sizee++;
-        }
-        jenre=new char[sizee];
-        for(int i=0; i<sizee; i++){
-            jenre[i]=_jenre[i];
-        }
-    }
-    song(song& other){
-        length=other.length;
-
-        int sizee=0;
-        for(int i=0; other.artist[i]!='\0'; i++){
-            sizee++;
-        }
-        artist=new char[sizee];
-        for(int i=0; i<sizee; i++){
-            artist[i]=other.artist[i];
-        }
-        sizee=0;
-        for(int i=0; other.jenre[i]!='\0'; i++){
-            sizee++;
-        }
-        jenre=new char[sizee];
-        for(int i=0; i<sizee; i++){
-            jenre[i]=other.jenre[i];
-        }
-
-    }
-    song& operator=(song& other){
-        if(this != &other){
-            delete[] artist;
-            delete[] jenre;
-            length=other.length;
-            int sizee=0;
-            for(int i=0; other.artist[i]!='\0'; i++){
-                sizee++;
-            }
-            artist=new char[sizee];
-            for(int i=0; i<sizee; i++){
-                artist[i]=other.artist[i];
-            }
-            sizee=0;
-            for(int i=0; other.jenre[i]!='\0'; i++){
-                sizee++;
-            }
-            jenre=new char[sizee];
-            for(int i=0; i<sizee; i++){
-                jenre[i]=other.jenre[i];
-            }
-
-        }
-        return *this;
-    }
-
-    void printsong(){
-        cout<<"Length: "<<length<<" minutes"<<endl;
-        cout<<"Artist: "<<artist<<endl;
-        cout<<"Jenre: "<<jenre<<endl;
-    }
-    ~song(){
-        delete[] artist;
-        delete[] jenre;
+    virtual void alert();
+};
+class Printer: public ElectricalDevice{
+private:
+    int serial_number;
+    int price_toner;
+    bool is_toner_out;
+public:
+    void alert(){
+        cout<<"Printer "<<serial_number<<" is out of toner";
     }
 };
-class playlist{
+class Scanner: public ElectricalDevice{
 private:
-    song* lista;
-    int sizee;
-    int capacity;
+    int price_paper;
+    bool is_paper_empty;
 public:
-    playlist(){
-        sizee=0;
-        capacity=4;
-        lista= new song[capacity];
-    }
-    void resizee(){
-        capacity=capacity*2;
-        song* newlista;
-        newlista= new song[capacity];
-        for(int i=0; i<sizee; i++){
-            newlista[i]=lista[i];
-        }
-        delete [] lista;
-        lista=newlista;
-
-    }
-    void add(song added){
-        if(sizee+1>capacity){resizee();}
-        lista[sizee]=added;
-        sizee++;
-    }
-    void printplaylist(){
-        cout<<sizee;
-        for(int i=0; i<sizee; i++){
-            lista[i].printsong();
-            cout<<endl;
-        }
+    void alert(){
+        cout<<"Scanner is out of paper ";
     }
 };
+class Room{
+private:
+    ElectricalDevice* rom;
+    int current;
+public:
+    Room(int sizee){
+        current=0;
+        rom= new ElectricalDevice[sizee];
+    }
+    void addDevice(ElectricalDevice& added){
+        rom[current]=added;
+    }
+
+
+};
+
+
 
 int main(){
-    song habibi(2,"azis","mazno");
-    song hab(habibi);
-    song ha;
-    song h;
-    ha=hab;
-    //ha.printsong();
-    playlist p1;
-    p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);
-    p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);
-    p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);
-    p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);
-    p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);
-    p1.add(habibi);
-    p1.add(hab);
-    p1.add(h);
-    p1.printplaylist();
+    Room r1(5);
+
+
+
 
     return 0;
 }
